33d2d9ff672557f3dc1e088e7a570da2a9ccea40
mayurL <mayur5643@gmail.com>
2021-04-20 16:16:58 +0530

    headerCreated

---
+++ src/screens/login/Login.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './Login.css';
+
+const Header = function(){
+    return (
+        <div className="imagecolor">
+            Image Viewer
+        </div>
+    )
+}
+export default Header;
